54ebaa5eefffd66298cf41ea13f7632090105460
JANG <gorae930301@gmail.com>
2025-05-15 02:16:16 +0900

    feat: add method

--- src/main/java/com/example/redis_demo/service/RedisDatabaseService.java
+++ src/main/java/com/example/redis_demo/service/RedisDatabaseService.java
@@ -1,5 +1,8 @@
 package com.example.redis_demo.service;
 
+import java.util.List;
+import java.util.stream.Collector;
+
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,6 +31,21 @@ public class RedisDatabaseService {
 		FoodMenu foodMenu = foodMenuRepository.findById(foodId).orElseThrow(() -> new IllegalArgumentException("메뉴를 찾을 수 없습니다."));
 		return FoodMenuResponse.builder().name(foodMenu.getName()).price(foodMenu.getPrice()).build();
 	}
+	
+    public List<FoodMenuResponse> findByPriceGreaterThan(Long price) {
+        List<FoodMenu> menus = foodMenuRepository.findByPriceGreaterThan(price);
+        return menus.stream().map(m -> FoodMenuResponse.builder().name(m.getName()).price(m.getPrice()).build()).toList();
+    }
+
+    public List<FoodMenuResponse> findByPriceLessThan(Long price) {
+    	List<FoodMenu> menus = foodMenuRepository.findByPriceLessThan(price);
+    	return menus.stream().map(m -> FoodMenuResponse.builder().name(m.getName()).price(m.getPrice()).build()).toList();
+    }
+
+    public List<FoodMenuResponse> findByPriceBetween(Long minPrice, Long maxPrice) {
+    	List<FoodMenu> menus = foodMenuRepository.findByPriceBetween(minPrice, maxPrice);
+    	return menus.stream().map(m -> FoodMenuResponse.builder().name(m.getName()).price(m.getPrice()).build()).toList();
+    }
 	
 
 	
